f68828b07971bbe19de9ab51c2a3d8c1aea965b7
PranavMehrotra <87844730+PranavMehrotra@users.noreply.github.com>
2023-03-02 23:51:21 +0530

    Some changes

--- A4/dbms/accounts/urls.py
+++ A4/dbms/accounts/urls.py
@@ -15,7 +15,7 @@ urlpatterns=[
      path('admit_discharge/',views.handle_admit, name='handle_admit'),
      path('schedule_appointment/',views.schedule_appoint, name='schedule_appoint'),
      path('admission/',views.admit_patient.as_view(), name='patient_admit'),
-     
+     path('scheduler/',views.scheduler, name='scheduler'),
      path('prescribe_medication/', views.doctor_prescribe.as_view(), name='prescribe_medic'),
      path('',views.index, name='index'),
      path('admin_login/',views.login_admin, name='admin_login'),
